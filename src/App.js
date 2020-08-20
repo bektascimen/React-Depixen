092a35a8102ec1604dc9a079d695cffc01a06486
Bektaş Çimen <bektas.cimen@kartaca.com>
2020-08-20 11:00:36 +0300

    #Episode 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Field from "./Field";
+import { Card,CardGroup,Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            currentItem: {
+                title: '',
+                _text: '',
+                image: ''
+            }
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+    handleInput(e) {
+        var currentItem = {...this.state.currentItem}
+        currentItem[e.target.name] = e.target.value
+        this.setState({
+            currentItem
+        })
+    }
+    addItem(e) {
+        e.preventDefault();
+        const newItem = this.state.currentItem;
+        console.log(newItem)
+        if ( (newItem._text !== "") && (newItem.title !== "") && (newItem.image !== "")) {
+            const newItems = [...this.state.items, newItem];
+            this.setState({
+                items: newItems,
+                currentItem: {
+                    _text: '',
+                    title: '',
+                    image: ''
+                }
+            })
+        }
+    }
+    render() {
+        return (
+            <div>
+                <header>
+                    <div className="App">
+                        <div style={{display:"inline-block"}}>
+                        <form id="card" onSubmit={this.addItem}>
+                            <CardGroup className="cga">
+                                <Card>
+                                    <Card.Body>
+                                        <Card.Title>
+                                            <input type="text" name="title"
+                                                   value={this.state.currentItem.title}
+                                                   onChange={this.handleInput}
+                                                   placeholder="Başlık giriniz"
+                                            />
+                                        </Card.Title>
+                                        <Card.Text className="cta">
+                                            <textarea name="_text" id="_text" cols="30" rows="10" maxLength="444"
+                                                      value={this.state.currentItem._text}
+                                                      onChange={this.handleInput}
+                                                      placeholder="Maximum 444 karakterli metin giriniz."
+                                            >X</textarea>
+                                        </Card.Text>
+                                    </Card.Body>
+                                    <Card.Footer className="cfa">
+                                        <input type="file" name="image"
+                                               value={this.state.currentItem.image}
+                                               onChange={this.handleInput}
+                                        />
+                                    </Card.Footer>
+                                </Card>
+                            </CardGroup>
+
+                            <div>
+                                <Button variant="outline-primary" type="submit">Ekle</Button>
+                            </div>
+                        </form>
+                        </div>
+                        <Field items = {this.state.items??[]}> </Field>
+                    </div>
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/Field.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Field.css'
+import {Card, CardGroup} from "react-bootstrap";
+
+function Field(props) {
+    const items = props.items;
+    const listItems = items?.map((item, index) => {
+        return <div key={index} style={{display: "inline-block", margin: "20px"}}>
+            <CardGroup style={{width: "400px"}}>
+                <Card className="text-center">
+                    <Card.Body>
+                        <Card.Header>{item.title}</Card.Header>
+                        <Card.Text style={{height: "253px"}}>
+                            {item._text}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Card.Img variant="top" src={item.image} style={{height: "226px"}}/>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+        </div>
+    })
+    return listItems;
+}
+
+export default Field;
